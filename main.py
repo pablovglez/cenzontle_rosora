de7cc851fb8f4985d8a054b544573b5c0231bde6
Pablo Gonzalez <pablovglez@gmail.com>
2026-06-09 13:29:05 +0200

    wip: replace with bleak

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from gi.repository import GLib
 import threading
 import time
 import asyncio
+from bleak import BleakScanner, BlueZScannerArgs
+from bleak.backends.device import BLEDevice
+from bleak.backends.scanner import AdvertisementData
+import argparse
+import logging
+
+logger = logging.getLogger(__name__)
 
 AGENT_PATH = "/org/bluez/agent"
 
@@ -25,6 +32,7 @@ AGENT_XML = """
 CHARACTERISTIC_UUIDS_PREFIXES = [
     "123e4567-f289-0b12",
     "f980ab40-65f5-4467",
+    "00002902-0000-1000", # Notification
 ]
 
 class FixedPasskeyAgent:
@@ -44,14 +52,13 @@ class FixedPasskeyAgent:
 class CenzontleDevice:
     def __init__(self, address, props):
         self.address = address
-        self.name = props.get('Name', None)
-        self.appearance = props.get('Appearance', None)
-        self.paired = props.get('Paired', None)
-        self.bonded = props.get('Bonded', None)
-        self.connected = props.get('Connected', None)
-        self.uuids = props.get('UUIDs', None)
-        self.path = f"{props.get('Adapter', None)}/dev_{self.address.replace(':', '_')}"
-        self.gatt_map = {}
+        self.name = props.get('props', {}).get('Name', None)
+        self.appearance = props.get('props', {}).get('Appearance', None)
+        self.paired = props.get('props', {}).get('Paired', None)
+        self.bonded = props.get('props', {}).get('Bonded', None)
+        self.connected = props.get('props', {}).get('Connected', None)
+        self.uuids = props.get('props', {}).get('UUIDs', None)
+        self.path = props.get('props', {}).get('Path', None)
 
     def __str__(self):
         return f"CenzontleDevice(address={self.address}, name={self.name})"
@@ -73,12 +80,12 @@ class Adapter:
 
 
 class BleManager:
-    def __init__(self, prefered_devices=[]):
+    def __init__(self, adapter=None):
         # Need to unblock bluetooth via rfkill
         # rfkill list bluetooth
         # sudo rfkill unblock bluetooth
         self.bus = SystemBus()
-        self.adapters = self._fetch_adapters(prefered_devices)
+        self.adapter = {"adapter": adapter} if adapter else None
         self.devices = {}
         # busctl introspect org.bluez /org/bluez/hci0/ # busctl introspect org.bluez /org/bluez/hci0/dev_D8_3A_DD_37_52_03
 
@@ -161,7 +168,7 @@ class BleManager:
             except Exception:
                 pass
 
-    def scan_devices(self, adapter_key, callback=None, timeout=5, uuids_filter=None, clean_devices=False):
+    """def scan_devices(self, adapter_key, callback=None, timeout=5, uuids_filter=None, clean_devices=False):
         if not self.adapters or adapter_key not in self.adapters:
             return False
 
@@ -193,7 +200,28 @@ class BleManager:
                     device_obj = CenzontleDevice(address, device_props)
                     self.devices[device_obj.name] = device_obj
 
-        return True
+        return True"""
+
+    def register_device(self, device: BLEDevice, advertisement_data: AdvertisementData):
+        logger.info(
+            "addr: %s, details: %s, %r", device.address, device.details, advertisement_data
+        )
+        address = device.address
+        if address and address not in self.devices:
+            device_obj = CenzontleDevice(address, device.details)
+            self.devices[device_obj.name] = device_obj
+
+    async def scan_devices(self, timeout=5, uuids_filter=None, clean_devices=False):
+        scanner = BleakScanner(
+            self.register_device, uuids_filter, bluez={"adapter": "hci0"}
+        )
+
+        logger.info("Starting scanner")
+        async with scanner:
+            await asyncio.sleep(timeout)
+
+        logger.info(f"Devices: {self.devices}")
+
 
     def connect_devices(self):
         for key, device in self.devices.items():
@@ -209,8 +237,9 @@ class BleManager:
         managed = bluez.GetManagedObjects()
         for path, ifaces in managed.items():
             gatt_chars = ifaces.get("org.bluez.GattCharacteristic1")
+            print(gatt_chars)
             char_uuid = gatt_chars.get("UUID", None) if gatt_chars else None
-            if char_uuid is not None and char_uuid[:18] in CHARACTERISTIC_UUIDS_PREFIXES:
+            if char_uuid is not None and any(char_uuid.startswith(prefix) for prefix in CHARACTERISTIC_UUIDS_PREFIXES):
                 # Find in device list
                 device_name = f"CENZ-{char_uuid[-8:].upper()}"
                 if device_name in self.devices.keys():
@@ -218,25 +247,21 @@ class BleManager:
                         "Handle": gatt_chars.get("Handle", None),
                         "path": path
                     }
+            print("****")
+            print(ifaces.get("org.bluez.GattDescriptor1"))
         for device in self.devices.values():
             print(device.gatt_map)
 
     def send_command(self, device_name, char_uuid, command):
         if device_name in self.devices.keys():
-            path = f'{self.devices[device_name].path}/service0028/char0029'
-            device_bus = self.bus.get("org.bluez", path)
-            #device_bus = self.bus.get("org.bluez", self._get_device_path(self.adapters[0], self.devices[device_name].address))
-            print(f"Sending command {command} to {device_name} at path {self.devices[device_name].path}")
-            #print(device_bus.Introspect())
-            #device_bus.WriteValue("org.bluez.GattCharacteristic1.WriteValue", "aya{sv}", command, {})
-            device_bus.WriteValue(command, {})
             device = self.devices[device_name]
             if char_uuid in device.gatt_map.keys():
                 device_bus = self.bus.get("org.bluez", device.gatt_map[char_uuid].get('path'))
                 device_bus.WriteValue(command, {}) if device_bus else None
 
+    #def enable_notifications(self, device_name, char_uuid):
 
-if __name__ == '__main__':
+"""if __name__ == '__main__':
     #manager = BleManager(["00:1A:7D:DA:71:15"])
     # If no prefered devices are given, the first one will be used
     manager = BleManager()
@@ -255,10 +280,6 @@ if __name__ == '__main__':
                          clean_devices=False
     # clean_devices=True
     )
-
-    # Run async manager.connect_devices()
-    #asyncio.run(manager.connect_devices())
-    #asyncio.run(manager.send_command("CENZ-0F8D17B6", [0x09, 0x01, 0x01, 0x00, 0x0B]))
     print(manager.devices)
     manager.connect_devices()
     print(f"Connected !")
@@ -267,11 +288,43 @@ if __name__ == '__main__':
 
     # Demo: send command
 
-    manager.send_command("CENZ-0F8D17B6", [0x09, 0x01, 0x01, 0x00, 0x0B])
-    time.sleep(5)
-    manager.send_command("CENZ-0F8D17B6", [0x09, 0x01, 0x00, 0x00, 0x0A])
-    manager.send_command("CENZ-0F8D17B6", '123e4567-f289-0b12-d302-a4f00f8d17b6', [0x09, 0x01, 0x01, 0x00, 0x0B])
+    # subscribe to characteristic
+    manager.send_command("CENZ-0F8D17B6", '00002902-0000-1000-8000-00805f9b34fb', [0x00, 0x01])"""
+
+"""manager.send_command("CENZ-0F8D17B6", '123e4567-f289-0b12-d302-a4f00f8d17b6', [0x09, 0x01, 0x01, 0x00, 0x0B])
     time.sleep(2)
     manager.send_command("CENZ-0F8D17B6", '123e4567-f289-0b12-d302-a4f00f8d17b6', [0x09, 0x01, 0x00, 0x00, 0x0A])
-    time.sleep(2)
+    time.sleep(2)"""
     # Send command once connected
+
+service_uuid_filter = [
+        '123e4567-f289-0b12-d3f6-a4f00f8d17b6',
+        'f980ab40-65f5-4467-0000-7c9ebd0755ba',
+        'f980ab40-65f5-4467-0000-a4f00f8d17b4'
+    ]
+
+def simple_callback(device: BLEDevice, advertisement_data: AdvertisementData):
+    logger.info(
+        "addr: %s, name: %s, %r", device.address, device.name, advertisement_data
+    )
+
+async def main():
+    scanner = BleakScanner(
+        simple_callback, service_uuid_filter, bluez={"adapter":"hci0"}
+    )
+
+    while True:
+        logger.info("(re)starting scanner")
+        async with scanner:
+            await asyncio.sleep(5.0)
+
+if __name__ == "__main__":
+    log_level = logging.INFO
+    logging.basicConfig(
+        level=log_level,
+        format="%(asctime)-15s %(name)-8s %(levelname)s: %(message)s",
+    )
+    manager = BleManager(adapter="hci0")
+
+    #asyncio.run(main())
+    asyncio.run(manager.scan_devices(timeout=5, uuids_filter=service_uuid_filter))
