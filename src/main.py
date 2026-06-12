48b0f785b7339f578ac2f9c5a234d566ab498ffd
Pablo Gonzalez <pablovglez@gmail.com>
2026-06-12 15:56:25 +0200

    wip: coroutines in progress

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ PASSKEY = 123456
 CHARACTERISTIC_UUIDS_PREFIXES = [
     "123e4567-f289-0b12",
     "f980ab40-65f5-4467",
-    "00002902-0000-1000", # Notification
+    "00002902-0000-1000",  # Notification
 ]
 
 
@@ -36,6 +36,62 @@ class CenzontleDevice:
 
     def __repr__(self):
         return self.__str__()
+
+    def _compute_checksum(self, data):
+        # Compute simple 2-byte checksum
+        checksum = 0
+        for byte in data:
+            checksum += byte
+        return checksum % 256
+
+    def on_echo_command(self, kwargs):
+        payload = kwargs.get('payload', None)
+        if payload is None:
+            return None
+        # Check that payload is a bytearray
+        if not isinstance(payload, bytearray):
+            return None
+        command = bytearray([0x0A]) + payload
+        checksum = self._compute_checksum(command)
+        command.extend([(checksum >> 8) & 0xFF, checksum & 0xFF])
+        return command
+
+    def on_relay_command(self, kwargs):
+        # validate kwargs
+        relay_number = kwargs.get('relay_number', None)
+        relay_state = kwargs.get('relay_state', None)
+
+        if None in (relay_number, relay_state):
+            return None
+
+        if not isinstance(relay_number, int) or not isinstance(relay_state, bool):
+            return None
+
+        command = bytearray([0x09, relay_number & 0xFF, 0x01 if relay_state else 0x00])
+        checksum = self._compute_checksum(command)
+        command.extend([(checksum >> 8) & 0xFF, checksum & 0xFF])
+
+        return command
+
+    async def send_command(self, command, kwargs=None):
+        if self.client is None:
+            return False
+
+        command_api = {
+            "set_relay": self.on_relay_command,
+            "echo": self.on_echo_command,
+        }
+
+        command_handler = command_api.get(command, None)
+        if command_handler is None:
+            return False
+
+        command_data = command_handler(kwargs)
+        if command_data is None:
+            return False
+
+        await self.client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6', command_data)
+        return True
 
 
 class BleManager:
@@ -61,7 +117,7 @@ class BleManager:
         self._dbus_thread.start()
         logger.info("DBus event loop running in background thread.")
 
-    def _unregister_agent(self):
+    def unregister_agent(self):
         if self.agent_manager:
             try:
                 self.agent_manager.UnregisterAgent(AGENT_PATH)
@@ -93,6 +149,8 @@ class BleManager:
         logger.info(f"Devices: {self.devices}")
 
     async def connect_devices(self):
+        if not self.devices:
+            return
         for device in self.devices.values():
             if device.client is None:
                 try:
@@ -103,28 +161,40 @@ class BleManager:
                     if (e.dbus_error == "org.bluez.Error.ConnectionAttemptFailed" and
                             "Page Timeout" in e.dbus_error_details):
                         # End of life of this run, we will retry later
-                        logger.warning(f"Connection attempt failed for {device.name} ({e.dbus_error} - {e.dbus_error_details}), will retry later.")
+                        logger.warning(
+                            f"Connection attempt failed for {device.name} ({e.dbus_error} - {e.dbus_error_details}), will retry later.")
                         continue
                     raise
 
-
-                # Log the client.services:
+    async def test_connect_devices(self):
+        for device in self.devices.values():
+            # Log the client.services:
             logger.info("Services: %s", device.client.services.services)
             logger.info("Chars: %s", device.client.services.characteristics)
             for chars in device.client.services.characteristics.values():
                 logger.info("char val: %s", chars.uuid)
             # Enable notifications, not working right now
-            await device.client.start_notify('f980ab40-65f5-4467-0002-58e6c519989a', self.notify_callback)
+            await device.client.start_notify('123e4567-f289-0b12-d302-a4f00f8d17b6', self.notify_callback)
             # Send command
-            await device.client.write_gatt_char('f980ab40-65f5-4467-0002-58e6c519989a',
-                                         bytearray([0x0A, 0x30, 0x31, 0x32, 0x030, 0x00, 0xCD]))
+            await device.client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6',
+                                                bytearray([0x0A, 0x30, 0x31, 0x32, 0x030, 0x00, 0xCD]))
             await asyncio.sleep(2)
-            await device.client.write_gatt_char('f980ab40-65f5-4467-0002-58e6c519989a',
-                                         bytearray([0x09, 0x01, 0x01, 0x00, 0x0B]))
+            await device.client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6',
+                                                bytearray([0x09, 0x01, 0x01, 0x00, 0x0B]))
             await asyncio.sleep(2)
-            await device.client.write_gatt_char('f980ab40-65f5-4467-0002-58e6c519989a',
-                                         bytearray([0x09, 0x01, 0x00, 0x00, 0x0A]))
+            await device.client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6',
+                                                bytearray([0x09, 0x01, 0x00, 0x00, 0x0A]))
             await asyncio.sleep(2)
+            await device.send_command("set_relay", {"relay_number": 1, "relay_state": True})
+            await asyncio.sleep(2)
+            await device.send_command("set_relay", {"relay_number": 1, "relay_state": False})
+            for i in range(1, 24):
+                num = i % 4 + 1
+                logger.info(f"Sending command {num} to {device.name}")
+                await asyncio.sleep(0.25)
+                await device.send_command("set_relay", {"relay_number": num, "relay_state": True})
+                await asyncio.sleep(0.25)
+                await device.send_command("set_relay", {"relay_number": num, "relay_state": False})
 
     async def disconnect_devices(self):
         for device in self.devices.values():
@@ -133,6 +203,7 @@ class BleManager:
 
     def notify_callback(self, char_uuid, value):
         logger.info("Notification from %s: %s", char_uuid, value)
+
 
 """if __name__ == '__main__':
     #manager = BleManager(["00:1A:7D:DA:71:15"])
@@ -168,57 +239,89 @@ class BleManager:
     time.sleep(2)
     manager.send_command("CENZ-0F8D17B6", '123e4567-f289-0b12-d302-a4f00f8d17b6', [0x09, 0x01, 0x00, 0x00, 0x0A])
     time.sleep(2)"""
-    # Send command once connected
+# Send command once connected
 
 service_uuid_filter = [
-        '123e4567-f289-0b12-d3f6-a4f00f8d17b6',
-        'f980ab40-65f5-4467-0000-7c9ebd0755ba',
-        'f980ab40-65f5-4467-0000-58e6c519989a',
-        'f980ab40-65f5-4467-0000-a4f00f8d17b4'
-    ]
+    '123e4567-f289-0b12-d3f6-a4f00f8d17b6',
+    'f980ab40-65f5-4467-0000-7c9ebd0755ba',
+    'f980ab40-65f5-4467-0000-58e6c519989a',
+    'f980ab40-65f5-4467-0000-a4f00f8d17b4'
+]
+
 
 def simple_callback(device: BLEDevice, advertisement_data: AdvertisementData):
     logger.info(
         "addr: %s, name: %s, %r", device.address, device.name, advertisement_data
     )
 
-async def main():
-    manager = BleManager(adapter="hci0")
-    await manager.scan_devices(timeout=5, uuids_filter=service_uuid_filter)
-    await manager.connect_devices()
-    await manager.disconnect_devices()
-    try:
-        manager.UnregisterAgent(AGENT_PATH)
-    except Exception:
-        pass
-    # Pair device
-    """for device in manager.devices.values():
-        async with BleakClient(device.address, pair=True, timeout=30) as client:
-            try:
-                await asyncio.sleep(2)
-                logger.info("Connected: %s", client.is_connected)
-            finally:
-                try:
-                    manager.UnregisterAgent(AGENT_PATH)
-                except Exception:
-                    pass
-                # Log the client.services:
-                logger.info("Services: %s", client.services.services)
-                logger.info("Chars: %s", client.services.characteristics)
-                for chars in client.services.characteristics.values():
-                    logger.info("char val: %s", chars.uuid)
-                # Enable notifications, not working right now
-                await client.start_notify('123e4567-f289-0b12-d302-a4f00f8d17b6', manager.notify_callback)
-                # Send command
-                await client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6',
-                                             bytearray([0x0A, 0x30, 0x31, 0x32, 0x030, 0x00, 0xCD]))
-                await asyncio.sleep(2)
-                await client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6', bytearray([0x09, 0x01, 0x01, 0x00, 0x0B]))
-                await asyncio.sleep(2)
-                await client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6',
-                                             bytearray([0x09, 0x01, 0x00, 0x00, 0x0A]))
-                await asyncio.sleep(2)"""
 
+async def monitored_task(coro_func, *args, name, restart_delay=2, **kwargs):
+    """Run a task with monitoring and restart"""
+    restart_count = 0
+
+    while True:
+        try:
+            if restart_count == 0:
+                logging.info(f"Task {name} started")
+            restart_count += 1
+
+            await coro_func(*args, **kwargs)
+
+        except asyncio.CancelledError:
+            logging.info(f"Task {name} cancelled after {restart_count} runs")
+            break
+
+        except Exception as e:
+            logging.error(f"Task {name} crashed after {restart_count} runs: {e}")
+            logging.info(f"Task Restarting {name} in {restart_delay}s...")
+            await asyncio.sleep(restart_delay)
+
+
+async def main(manager):
+    """await manager.scan_devices(timeout=5, uuids_filter=service_uuid_filter)
+    await manager.connect_devices()
+    await manager.test_connect_devices()
+    await manager.disconnect_devices()"""
+    async with asyncio.TaskGroup() as tg:
+
+        # Create monitored tasks
+        task1 = tg.create_task(monitored_task(manager.scan_devices,
+                                              5, service_uuid_filter,
+                                              name="DeviceScanner",
+                                              restart_delay=5,
+                                              ))
+        task2 = tg.create_task(monitored_task(manager.connect_devices, name="ClientTask", restart_delay=5))
+        #task3 = tg.create_task(monitored_task(manager.test_connect_devices, name="TestConnectTask", restart_delay=5))
+
+
+        await asyncio.Event().wait()
+        # Pair device
+        """for device in manager.devices.values():
+            async with BleakClient(device.address, pair=True, timeout=30) as client:
+                try:
+                    await asyncio.sleep(2)
+                    logger.info("Connected: %s", client.is_connected)
+                finally:
+                    try:
+                        manager.UnregisterAgent(AGENT_PATH)
+                    except Exception:
+                        pass
+                    # Log the client.services:
+                    logger.info("Services: %s", client.services.services)
+                    logger.info("Chars: %s", client.services.characteristics)
+                    for chars in client.services.characteristics.values():
+                        logger.info("char val: %s", chars.uuid)
+                    # Enable notifications, not working right now
+                    await client.start_notify('123e4567-f289-0b12-d302-a4f00f8d17b6', manager.notify_callback)
+                    # Send command
+                    await client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6',
+                                                 bytearray([0x0A, 0x30, 0x31, 0x32, 0x030, 0x00, 0xCD]))
+                    await asyncio.sleep(2)
+                    await client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6', bytearray([0x09, 0x01, 0x01, 0x00, 0x0B]))
+                    await asyncio.sleep(2)
+                    await client.write_gatt_char('123e4567-f289-0b12-d302-a4f00f8d17b6',
+                                                 bytearray([0x09, 0x01, 0x00, 0x00, 0x0A]))
+                    await asyncio.sleep(2)"""
 
 if __name__ == "__main__":
     log_level = logging.INFO
@@ -226,7 +329,16 @@ if __name__ == "__main__":
         level=log_level,
         format="%(asctime)-15s %(name)-8s %(levelname)s: %(message)s",
     )
-    #manager = BleManager(adapter="hci1")
 
-    asyncio.run(main())
-    #asyncio.run(manager.scan_devices(timeout=5, uuids_filter=service_uuid_filter))
+    manager = BleManager(adapter="hci0")
+    # manager = BleManager(adapter="hci1")
+
+    try:
+        asyncio.run(main(manager))
+    except KeyboardInterrupt:
+        logging.info("Shutting down...")
+    except Exception:
+        manager.unregister_agent()
+        raise
+
+    # asyncio.run(manager.scan_devices(timeout=5, uuids_filter=service_uuid_filter))
